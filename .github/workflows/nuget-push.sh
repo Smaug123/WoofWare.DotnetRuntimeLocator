1c4779a87d674fe5fdef8577f25ac411f5669daf
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-06-17 22:06:33 +0100

    try-again

--- .github/workflows/nuget-push.sh
+++ .github/workflows/nuget-push.sh
@@ -8,7 +8,7 @@ echo "version=$PACKAGE_VERSION" >> "$GITHUB_OUTPUT"
 
 tmp=$(mktemp)
 
-if ! dotnet nuget push "$SOURCE_NUPKG" --api-key "$NUGET_API_KEY" --source https://api.nuget.org/v3/index.json --skip-duplicate > "$tmp" ; then
+if ! dotnet nuget push "$SOURCE_NUPKG" --api-key "$NUGET_API_KEY" --source https://api.nuget.org/v3/index.json > "$tmp" ; then
     cat "$tmp"
     if grep 'already exists at feed' "$tmp" ; then
         echo "result=skipped" >> "$GITHUB_OUTPUT"
@@ -18,5 +18,7 @@ if ! dotnet nuget push "$SOURCE_NUPKG" --api-key "$NUGET_API_KEY" --source https
         exit 1
     fi
 fi
+
+cat "$tmp"
 
 echo "result=published" >> "$GITHUB_OUTPUT"
