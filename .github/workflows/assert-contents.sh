e0e06582c5324af27936393c7a9e3dc148f5a044
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2024-06-17 22:09:38 +0100

    More pipeline fixes (#11)

--- .github/workflows/assert-contents.sh
+++ .github/workflows/assert-contents.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Unzipping version from NuGet"
+ls from-nuget.nupkg
 mkdir from-nuget && cp from-nuget.nupkg from-nuget/zip.zip && cd from-nuget && unzip zip.zip && cd - || exit 1
 
+echo "Unzipping version from local build"
+ls packed/
 mkdir from-local && cp packed/*.nupkg from-local/zip.zip && cd from-local && unzip zip.zip && cd - || exit 1
 
 find from-local -type f -exec sha256sum {} \; | sort > from-local.txt
