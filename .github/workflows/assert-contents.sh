98e3d1d59fac7d81481250edf378d32be904eee4
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-06-17 22:42:35 +0100

    Relativise paths

--- .github/workflows/assert-contents.sh
+++ .github/workflows/assert-contents.sh
@@ -2,13 +2,13 @@
 
 echo "Unzipping version from NuGet"
 ls from-nuget.nupkg
-mkdir from-nuget && cp from-nuget.nupkg from-nuget/zip.zip && cd from-nuget && unzip zip.zip && cd - || exit 1
+mkdir from-nuget && cp from-nuget.nupkg from-nuget/zip.zip && cd from-nuget && unzip zip.zip && rm zip.zip && cd - || exit 1
 
 echo "Unzipping version from local build"
 ls packed/
-mkdir from-local && cp packed/*.nupkg from-local/zip.zip && cd from-local && unzip zip.zip && cd - || exit 1
+mkdir from-local && cp packed/*.nupkg from-local/zip.zip && cd from-local && unzip zip.zip && rm zip.zip && cd - || exit 1
 
-find from-local -type f -exec sha256sum {} \; | sort > from-local.txt
-find from-nuget -type f -and -not -name '.signature.p7s' -exec sha256sum {} \; | sort > from-nuget.txt
+cd from-local && find . -type f -exec sha256sum {} \; | sort > ../from-local.txt && cd .. || exit 1
+cd from-nuget && find . -type f -and -not -name '.signature.p7s' -exec sha256sum {} \; | sort > ../from-nuget.txt && cd .. || exit 1
 
 diff from-local.txt from-nuget.txt
