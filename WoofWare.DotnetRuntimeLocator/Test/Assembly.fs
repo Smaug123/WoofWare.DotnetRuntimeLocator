cf249fd556aa8a899a3b7ae6a232d208288b73cc
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2025-09-16 22:23:15 +0100

    Case-insensitive roll-forward parsing (#121)

---
+++ WoofWare.DotnetRuntimeLocator/Test/Assembly.fs
@@ -0,0 +1,21 @@
+namespace WoofWare.DotnetRuntimeLocator.Test
+
+open System
+open System.IO
+open System.Reflection
+
+[<RequireQualifiedAccess>]
+module Assembly =
+    let getEmbeddedResource (assembly : Assembly) (name : string) : string =
+        let names = assembly.GetManifestResourceNames ()
+
+        let names =
+            names |> Seq.filter (fun s -> s.EndsWith (name, StringComparison.Ordinal))
+
+        use s =
+            names
+            |> Seq.exactlyOne
+            |> assembly.GetManifestResourceStream
+            |> fun s -> new StreamReader (s)
+
+        s.ReadToEnd ()
